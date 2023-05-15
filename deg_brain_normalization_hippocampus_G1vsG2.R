dcd3e8282a04688da6ef988786686e0290f1a605
laupierre <laupierre@users.noreply.github.com>
2023-05-15 17:20:49 +0200

    Update deg_brain_normalization_hippocampus_G1vsG2.R

--- deg_brain_normalization_hippocampus_G1vsG2.R
+++ deg_brain_normalization_hippocampus_G1vsG2.R
@@ -3,16 +3,12 @@ library (Seurat)
 
 brain <- readRDS ("brain_G2G1_groups.rds")
 
-# # extract SCT hormalized data
+
+########## SCT normalized data
+## extract SCT data
 counts <- as.matrix (brain[["SCT"]]$data [ ,WhichCells(brain, expression = location == "Hippocampus")])
 dim (counts)
 # 18827   229
-
-boxplot (apply (counts, 1, mean))
-counts <- counts[apply (counts, 1, mean) > 0.2, ]
-dim (counts)
-# 8568  229
-
 
 
 ## manual calculation of SCT log fold changes and wilcoxon test of SCT values
@@ -30,11 +26,22 @@ mean.s2 <- apply (counts.s, 1, mean)
 res <- cbind (data.frame (mean.s2), data.frame (mean.s1))
 res <-  cbind (res, data.frame (log.fold.change= res$mean.s2 - res$mean.s1))
 colnames (res)[3] <- "log.fold.change"
-head (res)
 
 boxplot (res$log.fold.change)
 abline (h=0)
 res.mean <- res
+
+
+boxplot (apply (counts, 1, mean))
+counts <- counts[apply (counts, 1, mean) > 0.2, ]
+dim (counts)
+# 8568  229
+
+meta <- meta[row.names (meta) %in% colnames (counts), ]
+stopifnot (row.names (meta) == colnames (counts))
+
+res.mean <- res.mean[row.names (res.mean) %in% row.names (counts), ]
+
 
 
 ## For wilcoxon test, see http://www.sthda.com/english/wiki/unpaired-two-samples-wilcoxon-test-in-r
@@ -44,15 +51,14 @@ counts.s2 <- counts[ ,grepl("G2", meta$group)]
 
 resl <- list ()
 for (i in (1:dim(counts.s1)[1])) {
-res <- wilcox.test(counts.s1[i, ], counts.s2[i ,], exact=FALSE)
-resl[[i]] <- res$p.value
+res <- cbind (data.frame (gene_name= row.names (counts)[i]), data.frame (p.value= wilcox.test(counts.s1[i, ], counts.s2[i ,], exact=FALSE)$p.value))
+resl[[i]] <- res
 }
 
 res <- do.call ("rbind", resl)
 colnames (res)[1] <- "pval.wilcox"
-res <- cbind (res.mean, res) 
+res <- merge (res.mean, res, by="row.names") 
 res$padj <- p.adjust (res$pval.wilcox, method="BH")
-head (res)
 
 table (res$padj < 0.05)
 #FALSE  TRUE 
