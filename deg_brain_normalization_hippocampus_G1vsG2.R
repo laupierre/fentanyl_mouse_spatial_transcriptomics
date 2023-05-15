61c6502b8dba9db7b5a8d5ec45363d252e80855f
laupierre <laupierre@users.noreply.github.com>
2023-05-15 16:39:53 +0200

    Update deg_brain_normalization_hippocampus_G1vsG2.R

--- deg_brain_normalization_hippocampus_G1vsG2.R
+++ deg_brain_normalization_hippocampus_G1vsG2.R
@@ -1,1 +1,36 @@
+library (Seurat)
 
+
+brain <- readRDS ("brain_G2G1_groups.rds")
+
+# # extract SCT hormalized data
+counts <- as.matrix (brain[["SCT"]]$data [ ,WhichCells(brain, expression = location == "Hippocampus")])
+dim (counts)
+# 18827   229
+
+boxplot (apply (counts, 1, mean))
+counts <- counts[apply (counts, 1, mean) > 0.2, ]
+dim (counts)
+# 8568  229
+
+
+## manual calculation of SCT log fold changes and wilcoxon test of SCT values
+
+meta <- brain [ ,WhichCells(brain, expression = location == "Hippocampus")]@meta.data
+idx <- match (row.names (meta), colnames (counts))
+meta <- meta[idx, ]
+stopifnot (row.names (meta) == colnames (counts))
+
+counts.s <- counts[ ,grepl("G1", meta$group)]
+mean.s1 <- apply (counts.s, 1, mean)
+counts.s <- counts[ ,grepl("G2", meta$group)]
+mean.s2 <- apply (counts.s, 1, mean)
+
+res <- cbind (data.frame (mean.s1), data.frame (mean.s2))
+res <-  cbind (res, data.frame (log.fold.change= res$mean.s2 - res$mean.s1))
+colnames (res)[3] <- "log.fold.change"
+head (res)
+
+
+counts.s <- data.frame (cbind (counts[i, ], data.frame (meta
+head (counts[1, ]
