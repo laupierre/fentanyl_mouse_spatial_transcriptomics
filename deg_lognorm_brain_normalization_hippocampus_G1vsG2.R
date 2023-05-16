5a283f10a12643ace7a3aa56e27eb2f54827cd49
laupierre <laupierre@users.noreply.github.com>
2023-05-16 11:25:31 +0200

    Update deg_lognorm_brain_normalization_hippocampus_G1vsG2.R

--- deg_lognorm_brain_normalization_hippocampus_G1vsG2.R
+++ deg_lognorm_brain_normalization_hippocampus_G1vsG2.R
@@ -1,1 +1,83 @@
+## See the scuttle library: https://bioconductor.org/packages/devel/bioc/vignettes/scuttle/inst/doc/norm.html
+
+library (Seurat)
+library (scuttle)
+library(scran)
+
+brain <- readRDS ("brain_G2G1_groups.rds")
+brain
+
+sce <- as.SingleCellExperiment(brain)
+
+# add expression filtering here
+
+clusters <- quickCluster(sce)
+
+sce <- computePooledFactors(sce, clusters=clusters)
+summary(sizeFactors(sce))
+#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 0.4400  0.8341  0.9848  1.0000  1.1655  1.6719
+
+
+# normalization of the entire brain
+sce <- logNormCounts(sce)
+counts <- logcounts (sce)
+
+meta <- colData (sce)
+#meta.s <- meta [meta$location == "Hippocampus", ]
+idx <- match (row.names (meta), colnames (counts))
+meta <- meta[idx, ]
+stopifnot (row.names (meta) == colnames (counts))
+
+counts.s <- counts[ ,grepl("G1", meta$group)]
+mean.s1 <- apply (counts.s, 1, mean)
+counts.s <- counts[ ,grepl("G2", meta$group)]
+mean.s2 <- apply (counts.s, 1, mean)
+
+res <- cbind (data.frame (mean.s2), data.frame (mean.s1))
+res <-  cbind (res, data.frame (log.fold.change= res$mean.s2 - res$mean.s1))
+colnames (res)[3] <- "log.fold.change"
+
+boxplot (res$log.fold.change)
+abline (h=0)
+res.mean <- res
+
+
+
+## For wilcoxon test, see http://www.sthda.com/english/wiki/unpaired-two-samples-wilcoxon-test-in-r
+
+counts.s1 <- counts[ ,grepl("G1", meta$group)]
+counts.s2 <- counts[ ,grepl("G2", meta$group)]
+
+resl <- list ()
+for (i in (1:dim(counts.s1)[1])) {
+res <- cbind (data.frame (gene_name= row.names (counts)[i]), data.frame (p.value= wilcox.test(counts.s1[i, ], counts.s2[i ,], exact=FALSE)$p.value))
+resl[[i]] <- res
+}
+
+res <- do.call ("rbind", resl)
+colnames (res)[2] <- "pval.wilcox"
+res <- merge (res.mean, res, by.x="row.names", by.y="gene_name") 
+res$padj <- p.adjust (res$pval.wilcox, method="BH")
+res <- res[order (res$padj), ]
+colnames (res)[1] <- "gene_name"
+head (res)
+
+table (res$padj < 0.05)
+#FALSE  TRUE 
+#14007  2637 
+
+boxplot (res$log.fold.change)
+abline (h=0)
+
+
+
+
+
+
+
+
+
+
+
 
