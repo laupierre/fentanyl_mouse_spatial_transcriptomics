f517b3870fc39c90e4675c9343125d975e18518a
laupierre <laupierre@users.noreply.github.com>
2023-05-17 16:39:58 +0200

    Update deg_comparison_bulk_pseudobulk_single-cells.R

--- deg_comparison_bulk_pseudobulk_single-cells.R
+++ deg_comparison_bulk_pseudobulk_single-cells.R
@@ -4,41 +4,43 @@ library (openxlsx)
 bulk <- read.xlsx ("sham_vs_sni_Differential_Expression.xlsx")
 colnames (bulk)[2] <- "gene_name"
 
-## Pseudobulk RNA-Seq
-pseudo <- read.xlsx ("pseudobulk_hippocampus_selected_cells.xlsx")
+## Pseudobulk
+pseudo <- read.xlsx ("pseudobulk_hippocampus_lognorm_selected_cells.xlsx")
 pseudo <- pseudo[ ,c("gene_name", "logFC", "adj.P.Val", "AveExpr")]
 
-## Single-cell (from spatial transcriptomics)
-sc <- read.xlsx ("hippocampus_selected_cells_wilcoxon_analysis.xlsx")
+## Single-cell (from lognorm spatial transcriptomics)
+sc <- read.xlsx ("table 1. hippocampus_G2vsG1_selected_cells_normalization_wilcoxon_analysis.xlsx")
 # invert the log fold changes
-sc$avg_logFC <- -1*sc$avg_logFC
+#sc$avg_logFC <- -1*sc$avg_logFC
 sc <- sc[ ,c("gene_name", "avg_logFC", "p_val_adj", "mean", "Description")]
 
 
-pdf ("Comparison between bulk, pseudobulk and spatial RNA-Seq.pdf")
+pdf ("Comparison between bulk, pseudobulk and spatial RNA-Seq version 2.pdf")
 par (mfrow= c(3,1))
 comp1 <- merge (sc, pseudo, by="gene_name")
-plot (comp1$avg_logFC, comp1$logFC, xlab="log fold changes wilcoxon (spatial)", ylab="log fold changes limma (pseudobulk)", main="Comparison spatial vs pseudobulk transcriptomics",
+plot (comp1$avg_logFC, comp1$logFC, xlab="logFC wilcoxon (lognorm selected cells)", ylab="logFC limma (pseudobulk)", main="Comparison spatial vs pseudobulk transcriptomics",
       xlim=c(-3,3), ylim=c(-3,3))
 abline (0,1, col="red")
 abline (h=0)
 abline (v=0)
 
 comp2 <- merge (sc, bulk, by="gene_name")
-plot (comp2$avg_logFC, comp2$Log2.Fold.Change, xlab="log fold changes wilcoxon (spatial)", ylab="log fold changes wald (bulk)", main="Comparison spatial vs bulk transcriptomics",
+plot (comp2$avg_logFC, comp2$Log2.Fold.Change, xlab="logFC wilcoxon (lognorm selected cells)", ylab="logFC Bulk RNA-Seq", main="Comparison spatial vs bulk transcriptomics",
      xlim=c(-3,3), ylim=c(-3,3))
 abline (0,1, col="red")
 abline (h=0)
 abline (v=0)
 
 comp3 <- merge (pseudo, bulk, by="gene_name")
-plot (comp3$logFC, comp3$Log2.Fold.Change, xlab="log fold changes limma (pseudobulk)", ylab="log fold changes wald (bulk)", main="Comparison pseudobulk vs bulk transcriptomics",
+plot (comp3$logFC, comp3$Log2.Fold.Change, xlab="logFC limma (pseudobulk)", ylab="logfold Bulk RNA-Seq", main="Comparison pseudobulk vs bulk transcriptomics",
      xlim=c(-3,3), ylim=c(-3,3))
 abline (0,1, col="red")
 abline (h=0)
 abline (v=0)
 dev.off ()
 
+
+# Internal table comparison
 
 comp1 <- merge (sc, pseudo, by="gene_name")
 comp2 <- merge (comp1, bulk, by="gene_name")
