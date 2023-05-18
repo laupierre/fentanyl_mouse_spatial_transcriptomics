4ad1a186b0f58f45fbf013649cf74c7ca8886c64
laupierre <laupierre@users.noreply.github.com>
2023-05-18 12:46:38 +0200

    Update 10. add_gene_G2vsG1_in_image_plot_v2.R

--- 10. add_gene_G2vsG1_in_image_plot_v2.R
+++ 10. add_gene_G2vsG1_in_image_plot_v2.R
@@ -1,1 +1,19 @@
+library (Seurat)         # Seurat_4.9.9.9041
+library (ggpubr)
+library (sctransform)    # sctransform_0.3.5
 
+### Slide 1
+brain <- readRDS ("brain_slide1_G1G3_groups.rds")
+
+### Slide 2
+brain2 <- readRDS ("brain_slide2_G2G4_groups.rds")
+
+brain <- merge(brain, y = c(brain2))
+
+DefaultAssay(brain) <- "Spatial"
+brain <- JoinLayers (brain)
+
+# Here we normalize everything with SCT
+brain <- SCTransform(brain, vst.flavor = "v2") 
+
+Images (brain)
