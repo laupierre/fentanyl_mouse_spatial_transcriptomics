a0bca70887e9e936cedba3953b9a14c306a445cd
laupierre <laupierre@users.noreply.github.com>
2023-05-24 13:37:08 +0200

    Update 2. deg_selected_cells_hippocampus_G3vsG1.R

--- 2. deg_selected_cells_hippocampus_G3vsG1.R
+++ 2. deg_selected_cells_hippocampus_G3vsG1.R
@@ -13,9 +13,63 @@ counts <- counts[apply (counts, 1, mean) > 0.05, ]
 dim (counts)
 # 12691   252
 
-## FIXME
-# Dropout analysis
 
+
+#### Optional: Dropout removal
+
+meta <- brain@meta.data
+meta.g1 <- meta[grep ("G1", meta$group), ]
+meta.g1 <- meta.g1 [row.names (meta.g1) %in% colnames (counts), ]
+table (meta.g1$group)
+
+meta.g2 <- meta[grep ("G3", meta$group), ]
+meta.g2 <- meta.g2 [row.names (meta.g2) %in% colnames (counts), ]
+table (meta.g2$group)
+
+
+counts.g1 <- counts[ ,colnames (counts) %in% row.names (meta.g1)]
+dim (counts.g1)
+# 12691   120
+counts.g2 <- counts[ ,colnames (counts) %in% row.names (meta.g2)]
+dim (counts.g2)
+# 12691   132
+
+# Percentage of cells with zeros for each gene
+prop1 <- data.frame (prop1= apply (counts.g1, 1, function (x) {sum (x == 0)}))
+prop2 <- data.frame (prop2= apply (counts.g2, 1, function (x) {sum (x == 0)}))
+prop <- cbind (prop1/dim (counts.g1)[2] *100, prop2/dim (counts.g2)[2] *100)
+head (prop)
+
+
+## see: https://www.jdatalab.com/data_science_and_data_mining/2017/01/30/data-binning-plot.html
+
+library (ggplot2)
+library (ggpubr)
+
+p1 <- ggplot(data = prop, mapping = aes(x=prop1)) + 
+  geom_histogram(aes(y=..density..),fill="bisque",color="white",alpha=0.7, bins=20)  +
+  geom_density() + xlab ("Percentage of zeros") + ylab ("Density of expressed genes") +  geom_vline(xintercept=75, linetype="dashed", color = "red") +
+  ggtitle ("Percentage of zeros in the G1 group")
+  
+p2 <- ggplot(data = prop, mapping = aes(x=prop2)) + 
+  geom_histogram(aes(y=..density..),fill="bisque",color="white",alpha=0.7, bins=20)  +
+  geom_density() + xlab ("Percentage of zeros") + ylab ("Density of expressed genes") + geom_vline(xintercept=75, linetype="dashed", color = "red") +
+  ggtitle ("Percentage of zeros in the G3 group") 
+  
+p3 <- ggarrange (p1, p2, nrow=1)
+p3
+#ggsave ("figure 11. Percentage of dropouts in G1G3 groups.pdf", p3, width=8, height=8)
+
+
+# select genes with up to 75% dropouts
+prop$drop <- apply (prop, 1, function (x) {any (x > 75)})
+prop <- prop[prop$drop == FALSE, ]
+
+counts <- counts[row.names (counts) %in% row.names (prop), ]
+dim (counts)
+# 8865  252
+
+#### End of optional: Dropout removal
 
 
 
