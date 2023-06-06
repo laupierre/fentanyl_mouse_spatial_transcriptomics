24ada659656b4f889df406b9939404a5f6dc6495
laupierre <laupierre@users.noreply.github.com>
2023-06-06 08:54:01 +0200

    Create 11e. Deconvolution locations of all spots.R

---
+++ 11e. Deconvolution locations of all spots.R
@@ -0,0 +1,44 @@
+library (Seurat)
+
+options(Seurat.object.assay.version = "v5")
+
+data.dir <- "/Volumes/texas/iit_projects/martina/Northwestern University/NUSeq Core Facility - Martina03_9.16.2021/Space Ranger Output/G2-2C"
+#meta <- read.delim ("/Volumes/texas/iit_projects/martina/Northwestern University/NUSeq Core Facility - Martina03_9.16.2021/WORKING/Location information/G2_2C_sniv02.csv", sep=",")
+meta <- read.delim ("/Volumes/texas/iit_projects/martina/Northwestern University/NUSeq Core Facility - Martina03_9.16.2021/Space Ranger Output/G1-1A/cell_proportions_allen.txt")
+
+
+sample.name <- gsub (".*/", "", data.dir)
+brain <- Load10X_Spatial (data.dir, filename= "filtered_feature_bc_matrix.h5")
+brain[["Spatial"]] <- as(brain[["Spatial"]], Class = "Assay5")
+
+# Remove cells with mitochondrial contamination
+brain <- PercentageFeatureSet(brain, "^mt-", col.name = "percent_mito")
+brain <- PercentageFeatureSet(brain, "^Hb.*-", col.name = "percent_hb")
+brain <- brain[ ,brain$nFeature_Spatial > 500 & brain$percent_mito < 20 & brain$percent_hb < 20]
+
+# remove mitochondrial genes
+brain <- brain[!grepl("^mt-", rownames(brain)), ]
+# remove Hemoglobin genes (if that is a problem on your data)
+brain <- brain[!grepl("^Hb.*-", rownames(brain)), ]
+
+# normalization
+#brain <- SCTransform(brain, assay = "Spatial", verbose = FALSE)
+# SpatialFeaturePlot(brain, features = c("Hpca", "Ttr"))
+
+colnames (meta)[1] <- "Barcode"
+colnames (meta)[2] <- "TOTAL"
+meta1 <- meta
+print (dim (meta1))
+
+cells.use <- meta1$Barcode
+cells.use <- cells.use[cells.use %in% row.names (brain@meta.data)]
+idx <- match (cells.use, row.names (brain@meta.data))
+brain@meta.data$location [idx] <- meta1$cell_type1[idx]
+brain@meta.data$group <- sample.name
+brain@meta.data$cell <- paste (row.names(brain@meta.data), sample.name, sep="-")
+
+brain <- UpdateSeuratObject(brain)
+return (brain)
+
+
+
