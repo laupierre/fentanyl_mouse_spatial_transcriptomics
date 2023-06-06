969e93814c2cc58d94e53ebfe3cf12bec7717d92
laupierre <laupierre@users.noreply.github.com>
2023-06-06 10:36:55 +0200

    Update 11e. Deconvolution locations of all spots.R

--- 11e. Deconvolution locations of all spots.R
+++ 11e. Deconvolution locations of all spots.R
@@ -3,7 +3,6 @@ library (Seurat)
 options(Seurat.object.assay.version = "v5")
 
 data.dir <- "/Volumes/texas/iit_projects/martina/Northwestern University/NUSeq Core Facility - Martina03_9.16.2021/Space Ranger Output/G1-1A"
-#meta <- read.delim ("/Volumes/texas/iit_projects/martina/Northwestern University/NUSeq Core Facility - Martina03_9.16.2021/WORKING/Location information/G2_2C_sniv02.csv", sep=",")
 meta <- read.delim ("/Volumes/texas/iit_projects/martina/Northwestern University/NUSeq Core Facility - Martina03_9.16.2021/Space Ranger Output/G1-1A/cell_proportions_allen.txt")
 
 
@@ -21,18 +20,21 @@ brain <- brain[!grepl("^mt-", rownames(brain)), ]
 # remove Hemoglobin genes (if that is a problem on your data)
 brain <- brain[!grepl("^Hb.*-", rownames(brain)), ]
 
-# normalization
-#brain <- SCTransform(brain, assay = "Spatial", verbose = FALSE)
-# SpatialFeaturePlot(brain, features = c("Hpca", "Ttr"))
-
 colnames (meta)[1] <- "Barcode"
 meta1 <- meta
 print (dim (meta1))
 
 tokeep <- row.names (brain@meta.data) [row.names (brain@meta.data) %in% meta1$Barcode]
 brain <- subset (brain, cells = tokeep)
+dim (brain[["Spatial"]]$counts)
+
+
+
+
 idx <- match (tokeep, row.names (brain@meta.data))
-brain@meta.data$allen <- meta1$cell_type1[idx]
+brain@meta.data$allen <- meta1$Barcode[idx]
+table (brain@meta.data$allen == row.names (brain@meta.data))
+
 brain@meta.data$allen <- gsub ("=.*","", brain@meta.data$allen)
 brain@meta.data$group <- sample.name
 brain@meta.data$cell <- paste (row.names(brain@meta.data), sample.name, sep="-")
