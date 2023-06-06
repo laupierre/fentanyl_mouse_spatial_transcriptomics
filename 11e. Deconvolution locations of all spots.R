6c6478a6f70f108b2524ad0e847fba2aaedd1239
laupierre <laupierre@users.noreply.github.com>
2023-06-06 11:19:04 +0200

    Update 11e. Deconvolution locations of all spots.R

--- 11e. Deconvolution locations of all spots.R
+++ 11e. Deconvolution locations of all spots.R
@@ -22,33 +22,30 @@ brain <- brain[!grepl("^Hb.*-", rownames(brain)), ]
 
 colnames (meta)[1] <- "Barcode"
 meta1 <- meta
-print (dim (meta1))
 
-tokeep <- row.names (brain@meta.data) [row.names (brain@meta.data) %in% meta1$Barcode]
+tokeep <- intersect (meta1$Barcode, row.names (brain@meta.data))
 brain <- subset (brain, cells = tokeep)
 dim (brain[["Spatial"]]$counts)
 
-
-
-
+meta1 <- meta1[meta1$Barcode %in% tokeep, ]
 idx <- match (tokeep, row.names (brain@meta.data))
-brain@meta.data$allen <- meta1$Barcode[idx]
-table (brain@meta.data$allen == row.names (brain@meta.data))
-
+#brain@meta.data$allen <- meta1$Barcode[idx]
+#table (brain@meta.data$allen == row.names (brain@meta.data))
+brain@meta.data$allen <- meta1$cell_type1[idx]
 brain@meta.data$allen <- gsub ("=.*","", brain@meta.data$allen)
 brain@meta.data$group <- sample.name
 brain@meta.data$cell <- paste (row.names(brain@meta.data), sample.name, sep="-")
 
 brain <- UpdateSeuratObject(brain)
-return (brain)
 
 Idents (brain) <- "allen"
 SpatialDimPlot(brain)
 #SpatialDimPlot(brain, group.by = c("allen"))
 
-dg <- subset(brain, idents = c("DG"))
-SpatialDimPlot(dg)
-
+hip <- subset(brain, idents = c("CA1-ProS", "CA2-IG-FC", "CA3", "DG"))
+SpatialDimPlot(hip, crop=FALSE, label = FALSE, pt.size.factor = 0.8, alpha = 0.9)
+#ggsave("preselected_cells.jpg", height=8, width=8, units='in', dpi=300)
+ggsave("preselected_cells_normal_slice.pdf", height=8, width=8)
 
 
 
