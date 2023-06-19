68e58029b6a25693bbdd15bf224472e94d586483
laupierre <laupierre@users.noreply.github.com>
2023-06-19 18:26:04 +0200

    Update 9. add_gene_G2vsG1_in_selected_cells_only_image_plot.R

--- 9. add_gene_G2vsG1_in_selected_cells_only_image_plot.R
+++ 9. add_gene_G2vsG1_in_selected_cells_only_image_plot.R
@@ -13,12 +13,6 @@ Images (brain)
 # "slice1"   "slice1.2" "slice1.3" "slice1.4"
 # G2-2C, G2-2A, G1-1C, G1-1A
 
-# subset to the selected hippocampal spots only
-
-Idents (brain) <- "location"
-brain <- subset (brain, idents = c("Hippocampus"))
-
-
 
 ## without cropping
 
@@ -44,7 +38,7 @@ pall1 <- ggarrange (pa1, pa2, nrow=2)
 return (pall1)
 }
 
-gene <- "Plppr4"
+gene <- "Ttr"
 pall3 <- add_image (gene)
 pall3
 
@@ -54,19 +48,24 @@ ggsave (paste (gene, " version 3 plot.pdf", sep=""), pall3, width=8, height=8)
 
 ## with cropping, the figure is stitched, see https://github.com/satijalab/seurat/issues/5212
 
+## subset to the selected hippocampal spots only
+Idents (brain) <- "location"
+brain.s <- subset (brain, idents = c("Hippocampus"))
+
+
 add_image <- function (gene) {
-max_gene <- max (brain[["SCT"]]$data [row.names (brain[["SCT"]]$data) == gene, ])
+max_gene <- max (brain.s[["SCT"]]$data [row.names (brain.s[["SCT"]]$data) == gene, ])
 midpoint <- max_gene /2
 
 
-p1 <- SpatialFeaturePlot(brain, images=c("slice1"), crop=TRUE, pt.size.factor = 4, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene),
+p1 <- SpatialFeaturePlot(brain.s, images=c("slice1"), crop=TRUE, pt.size.factor = 4, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene),
 						 breaks = round (seq(0, max_gene, length.out = 6), digits=1))
-p2 <- SpatialFeaturePlot(brain, images=c("slice1.2"), crop=TRUE, pt.size.factor = 4, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene), 
+p2 <- SpatialFeaturePlot(brain.s, images=c("slice1.2"), crop=TRUE, pt.size.factor = 4, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene), 
 						 breaks = round (seq(0, max_gene, length.out = 6), digits=1))
 
-p3 <- SpatialFeaturePlot(brain, images=c("slice1.3"), crop=TRUE, pt.size.factor = 4, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene),
+p3 <- SpatialFeaturePlot(brain.s, images=c("slice1.3"), crop=TRUE, pt.size.factor = 4, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene),
 						 breaks = round (seq(0, max_gene, length.out = 6), digits=1))
-p4 <- SpatialFeaturePlot(brain, images=c("slice1.4"), crop=TRUE, pt.size.factor = 4, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene), 
+p4 <- SpatialFeaturePlot(brain.s, images=c("slice1.4"), crop=TRUE, pt.size.factor = 4, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene), 
 						 breaks = round (seq(0, max_gene, length.out = 6), digits=1))
 
 pa1 <- ggarrange (p2 | p1, nrow=1, labels="G2")
@@ -76,7 +75,7 @@ pall1 <- ggarrange (pa1, pa2, nrow=2)
 return (pall1)
 }
 
-gene <- "Plppr4"
+gene <- "Ttr"
 pall3 <- add_image (gene)
 pall3
 
