4bae94d516291f661b215e64c02603fabeb23e05
laupierre <laupierre@users.noreply.github.com>
2023-06-23 14:36:51 +0200

    Update 2. deg_selected_cells_hippocampus_G4vsG2.R

--- 2. deg_selected_cells_hippocampus_G4vsG2.R
+++ 2. deg_selected_cells_hippocampus_G4vsG2.R
@@ -5,9 +5,13 @@ library (openxlsx)
 brain <- readRDS ("brain_slide2_G2G4_groups.rds")
 
 # raw counts
-counts <- as.matrix (brain[["SCT"]]$counts [ ,WhichCells(brain, expression = location == "Hippocampus")])
-dim (counts)
+#counts <- as.matrix (brain[["SCT"]]$counts [ ,WhichCells(brain, expression = location == "Hippocampus")])
+#dim (counts)
 # 18637   211
+
+myarea <- "DG"
+counts <- as.matrix (brain[["Spatial"]]$counts [ ,WhichCells(brain, expression = location == myarea)])
+dim (counts)
 
 counts <- counts[apply (counts, 1, mean) > 0.05, ]
 dim (counts)
@@ -17,15 +21,73 @@ dim (counts)
 
 #### Optional: Dropout removal
 
+## Step1
+
+meta <- brain@meta.data
+
+meta.g1 <- meta[grep ("G2-2A", meta$group), ]
+meta.g1 <- meta.g1 [row.names (meta.g1) %in% colnames (counts), ]
+table (meta.g1$group)
+
+meta.g2 <- meta[grep ("G4-2B", meta$group), ]
+meta.g2 <- meta.g2 [row.names (meta.g2) %in% colnames (counts), ]
+table (meta.g2$group)
+
+meta.g3 <- meta[grep ("G2-2C", meta$group), ]
+meta.g3 <- meta.g3 [row.names (meta.g3) %in% colnames (counts), ]
+table (meta.g3$group)
+
+meta.g4 <- meta[grep ("G4-2D", meta$group), ]
+meta.g4 <- meta.g4 [row.names (meta.g4) %in% colnames (counts), ]
+table (meta.g4$group)
+
+
+counts.g1 <- counts[ ,colnames (counts) %in% row.names (meta.g1)]
+dim (counts.g1)
+counts.g2 <- counts[ ,colnames (counts) %in% row.names (meta.g2)]
+dim (counts.g2)
+counts.g3 <- counts[ ,colnames (counts) %in% row.names (meta.g3)]
+dim (counts.g3)
+counts.g4 <- counts[ ,colnames (counts) %in% row.names (meta.g4)]
+dim (counts.g4)
+
+# Number of cells that express a gene (for all genes)
+prop1 <- data.frame (G2_2A= apply (counts.g1, 1, function (x) {sum (x != 0)}))
+prop2 <- data.frame (G4_2B= apply (counts.g2, 1, function (x) {sum (x != 0)}))
+prop3 <- data.frame (G2_2C= apply (counts.g3, 1, function (x) {sum (x != 0)}))
+prop4 <- data.frame (G4_2D= apply (counts.g4, 1, function (x) {sum (x != 0)}))
+numa <- cbind (prop1, prop2, prop3, prop4)
+
+# Add the two numbers of cells
+prop <- cbind ( data.frame (G2= prop1+prop3), data.frame (G4= prop2+prop4))
+colnames (prop) <- c("G2","G4")
+
+numa <- cbind (numa, prop)
+
+# Percentage of expressing cells in a group
+prop2 <- data.frame (G2_prop= prop$G2 / (dim (counts.g1) [2] + dim (counts.g3) [2]))
+prop3 <- data.frame (G4_prop= prop$G4 / (dim (counts.g2) [2] + dim (counts.g4) [2]))
+prop <- round (cbind (prop2 *100, prop3*100), digits=1)
+
+numa <- cbind (numa, prop)
+head (numa)
+
+
+## Step2 
 meta <- brain@meta.data
 meta.g1 <- meta[grep ("G2", meta$group), ]
 meta.g1 <- meta.g1 [row.names (meta.g1) %in% colnames (counts), ]
+table (meta.g1$group)
 
 meta.g2 <- meta[grep ("G4", meta$group), ]
 meta.g2 <- meta.g2 [row.names (meta.g2) %in% colnames (counts), ]
+table (meta.g2$group)
+
 
 counts.g1 <- counts[ ,colnames (counts) %in% row.names (meta.g1)]
+dim (counts.g1)
 counts.g2 <- counts[ ,colnames (counts) %in% row.names (meta.g2)]
+dim (counts.g2)
 
 
 # Percentage of cells with zeros
@@ -49,10 +111,10 @@ p2 <- ggplot(data = prop, mapping = aes(x=prop2)) +
   geom_histogram(aes(y=..density..),fill="bisque",color="white",alpha=0.7, bins=20)  +
   geom_density() + xlab ("Percentage of zeros") + ylab ("Density of expressed genes") + geom_vline(xintercept=75, linetype="dashed", color = "red") +
   ggtitle ("Percentage of zeros in the G4 group") 
-  
+
 p3 <- ggarrange (p1, p2, nrow=1)
 p3
-#ggsave ("figure 12. Percentage of dropouts in G2G4 groups.pdf", p3, width=8, height=8)
+
 
 # Proportion of cells expressing a gene
 prop <- 100- prop
@@ -63,10 +125,8 @@ prop <- prop[prop$drop == TRUE, ]
 
 counts <- counts[row.names (counts) %in% row.names (prop), ]
 dim (counts)
-# 8546  211
 
-#### End of optional: Dropout removal
-
+#### End of optional step: Gene dropout removal
 
 
 
@@ -74,11 +134,11 @@ dim (counts)
 counts <- as (counts, "sparseMatrix")
 seurat.ss2 <- CreateSeuratObject(counts)
 
-meta.ss2 <- brain@meta.data[WhichCells(brain, expression = location == "Hippocampus"), ]
+meta.ss2 <- brain@meta.data[WhichCells(brain, expression = location == myarea), ]
 # we need cell_type (eg hippocampus), replicate (eg mouse), and label (eg treatment)
 meta.ss2$replicate <- gsub (".*-", "", meta.ss2$group)
 meta.ss2$label <- gsub ("-.*", "", meta.ss2$group)
-meta.ss2$cell_type <- "Hippocampus"
+meta.ss2$cell_type <- myarea
 
 meta.ss2 <- meta.ss2[row.names (meta.ss2) %in% colnames (counts), ]
 idx <- match (row.names (meta.ss2), colnames (counts))
@@ -91,29 +151,22 @@ table (seurat.ss2@meta.data$label)   # run_de will compare G2 vs G4
 #109 102 
 
 
-
 res <- run_de(seurat.ss2, de_method = 'wilcox', de_family= "singlecell")
 res <- data.frame (res)
 row.names (res) <- res$gene
-res <- merge (res, mymean, by="row.names")
-res <- res[order (res$p_val_adj), ]
-row.names (res) <- res$gene
-colnames (res)[1] <- "gene_name"
+# we want G3 vs G1
 res$avg_logFC <- -1 * res$avg_logFC
 
-
-## Add the cell proportion
-res <- merge (res,prop, by="row.names")
-res <- res[ ,-1]
-res <- res[ ,-dim (res)[2]]
-row.names (res) <- res$gene_name 
+## Add numa info
+res <- merge (res, numa, by.x="gene", by.y="row.names")
+row.names (res) <- res$gene 
+res <- res[order (res$p_val_adj), ]
 
 
 ## Annotation
 
 library('org.Mm.eg.db')
 
-#columns(org.Mm.eg.db)
 symbols <- row.names (res)
 res1a <- mapIds(org.Mm.eg.db, symbols, 'GENENAME', 'SYMBOL')
 
@@ -127,6 +180,13 @@ table (res$p_val_adj < 0.05)
 # 8510    36 
 
 write.xlsx (res, "table 3. hippocampus_G4vsG2_selected_cells_normalization_wilcoxon_analysis.xlsx", rowNames=F)
+
+boxplot (res$avg_logFC)
+abline (h=0)
+
+write.xlsx (res, paste (paste ("table 3.", myarea), "_area_G4vsG2_selected_cells_normalization_wilcoxon_analysis_with_percentage_cells.xlsx", sep=""), rowNames=F)
+
+
 
 
 
