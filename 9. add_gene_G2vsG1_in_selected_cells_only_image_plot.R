619736915b7ea388c40f64f5f58f82bcbc7643c3
laupierre <laupierre@users.noreply.github.com>
2023-06-26 14:46:43 +0200

    Update 9. add_gene_G2vsG1_in_selected_cells_only_image_plot.R

--- 9. add_gene_G2vsG1_in_selected_cells_only_image_plot.R
+++ 9. add_gene_G2vsG1_in_selected_cells_only_image_plot.R
@@ -142,36 +142,32 @@ rotation <- -90
 
 
 p1 <- SpatialFeaturePlot(brain, images=c("slice1"),  pt.size.factor = 1, crop=FALSE, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene),
-						 breaks = round (seq(0, max_gene, length.out = 6), digits=1)) + labs(title = "G2-2C") + theme(legend.text = element_text(angle=(-1*rotation), hjust=-2),
+			 breaks = round (seq(0, max_gene, length.out = 6), digits=1)) + labs(title = "G2-2C") + theme(legend.text = element_text(angle=(-1*rotation), hjust=-2),
                          legend.title = element_text(angle=(-1*rotation), hjust=0.8,  size=12), 
                          plot.title = element_text(angle=(-1*rotation), face = "bold", size=12), 
                          plot.caption = element_text(angle=(-1*rotation), vjust=0.5, size=1),
                          plot.caption.position = "plot")
                          
-
 p2 <- SpatialFeaturePlot(brain, images=c("slice1.2"),  pt.size.factor = 1, crop=FALSE, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene), 
-						 breaks = round (seq(0, max_gene, length.out = 6), digits=1)) + labs(title = "G2-2A") + theme(legend.text = element_text(angle=(-1*rotation), hjust=-2),
+			 breaks = round (seq(0, max_gene, length.out = 6), digits=1)) + labs(title = "G2-2A") + theme(legend.text = element_text(angle=(-1*rotation), hjust=-2),
                          legend.title = element_text(angle=(-1*rotation), hjust=0.8,  size=12), 
                          plot.title = element_text(angle=(-1*rotation), face = "bold", size=12), 
                          plot.caption = element_text(angle=(-1*rotation), vjust=0.5, size=1),
                          plot.caption.position = "plot")
 
-                         
 p3 <- SpatialFeaturePlot(brain, images=c("slice1.3"),  pt.size.factor = 1, crop=FALSE, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene),
-						 breaks = round (seq(0, max_gene, length.out = 6), digits=1)) + labs(title = "G1-1C") + theme(legend.text = element_text(angle=(-1*rotation), hjust=-2),
+			 breaks = round (seq(0, max_gene, length.out = 6), digits=1)) + labs(title = "G1-1C") + theme(legend.text = element_text(angle=(-1*rotation), hjust=-2),
                          legend.title = element_text(angle=(-1*rotation), hjust=0.8,  size=12), 
                          plot.title = element_text(angle=(-1*rotation), face = "bold", size=12), 
                          plot.caption = element_text(angle=(-1*rotation), vjust=0.5, size=1),
                          plot.caption.position = "plot")
 
-                         
 p4 <- SpatialFeaturePlot(brain, images=c("slice1.4"),  pt.size.factor = 1, crop=FALSE, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene), 
-						 breaks = round (seq(0, max_gene, length.out = 6), digits=1)) + labs(title = "G1-1A") + theme(legend.text = element_text(angle=(-1*rotation), hjust=-2),
+		         breaks = round (seq(0, max_gene, length.out = 6), digits=1)) + labs(title = "G1-1A") + theme(legend.text = element_text(angle=(-1*rotation), hjust=-2),
                          legend.title = element_text(angle=(-1*rotation), hjust=0.8,  size=12), 
                          plot.title = element_text(angle=(-1*rotation), face = "bold", size=12), 
                          plot.caption = element_text(angle=(-1*rotation), vjust=0.5, size=1),
                          plot.caption.position = "plot")
-                         
                          
 pa1 <- ggarrange (p2 | p1, nrow=1, labels="")
 pa2 <- ggarrange (p4 | p3, nrow=1, labels="")
@@ -183,7 +179,6 @@ return (pall1)
 
 gene <- "Ttr"
 pall3 <- add_image (gene)
-
 
 
 library (grid)
@@ -256,6 +251,87 @@ dev.off ()
 #pushViewport(viewport(angle=-90, width = unit(8, "inches"), height = unit(8, "inches")))  
 #grid.draw(ggplot_gtable(ggplot_build(pall3)))
 #dev.off ()
+
+
+
+## option 4 (version chosen): subset the selected hippocampal spots only and rotate the grid
+
+Idents (brain) <- "location"
+# brain <- subset (brain, idents = c("Hippocampus"))
+brain.s <- subset (brain, idents = myarea)
+
+## keep original colors of the entire brain (see below)
+
+add_image <- function (gene) {
+
+rotation <- -90
+
+p1 <- SpatialFeaturePlot(brain, images=c("slice1"),  pt.size.factor = 1, crop=FALSE, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene),
+			 breaks = round (seq(0, max_gene, length.out = 6), digits=1)) + labs(title = "G2-2C") + theme(legend.text = element_text(angle=(-1*rotation), hjust=-2),
+                         legend.title = element_text(angle=(-1*rotation), hjust=0.8,  size=12), 
+                         plot.title = element_text(angle=(-1*rotation), face = "bold", size=12), 
+                         plot.caption = element_text(angle=(-1*rotation), vjust=0.5, size=1),
+                         plot.caption.position = "plot")
+                         
+p2 <- SpatialFeaturePlot(brain, images=c("slice1.2"),  pt.size.factor = 1, crop=FALSE, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene), 
+			 breaks = round (seq(0, max_gene, length.out = 6), digits=1)) + labs(title = "G2-2A") + theme(legend.text = element_text(angle=(-1*rotation), hjust=-2),
+                         legend.title = element_text(angle=(-1*rotation), hjust=0.8,  size=12), 
+                         plot.title = element_text(angle=(-1*rotation), face = "bold", size=12), 
+                         plot.caption = element_text(angle=(-1*rotation), vjust=0.5, size=1),
+                         plot.caption.position = "plot")
+
+p3 <- SpatialFeaturePlot(brain, images=c("slice1.3"),  pt.size.factor = 1, crop=FALSE, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene),
+			 breaks = round (seq(0, max_gene, length.out = 6), digits=1)) + labs(title = "G1-1C") + theme(legend.text = element_text(angle=(-1*rotation), hjust=-2),
+                         legend.title = element_text(angle=(-1*rotation), hjust=0.8,  size=12), 
+                         plot.title = element_text(angle=(-1*rotation), face = "bold", size=12), 
+                         plot.caption = element_text(angle=(-1*rotation), vjust=0.5, size=1),
+                         plot.caption.position = "plot")
+
+p4 <- SpatialFeaturePlot(brain, images=c("slice1.4"),  pt.size.factor = 1, crop=FALSE, alpha = c(0.8, 1), features = gene) + ggplot2::scale_fill_gradient2(midpoint = midpoint, low="blue", mid="white", high="red", limits = c(0,max_gene), 
+		         breaks = round (seq(0, max_gene, length.out = 6), digits=1)) + labs(title = "G1-1A") + theme(legend.text = element_text(angle=(-1*rotation), hjust=-2),
+                         legend.title = element_text(angle=(-1*rotation), hjust=0.8,  size=12), 
+                         plot.title = element_text(angle=(-1*rotation), face = "bold", size=12), 
+                         plot.caption = element_text(angle=(-1*rotation), vjust=0.5, size=1),
+                         plot.caption.position = "plot")
+                         
+pa1 <- ggarrange (p2 | p1, nrow=1, labels="")
+pa2 <- ggarrange (p4 | p3, nrow=1, labels="")
+pall1 <- ggarrange (pa1, pa2, nrow=2)
+
+return (pall1)
+}
+
+
+
+gene <- "Ttr"
+
+## keep original colors of the entire brain
+max_gene <- max (brain[["SCT"]]$data [row.names (brain[["SCT"]]$data) == gene, ])
+midpoint <- max_gene /2
+
+pall3 <- add_image (gene, max_gene, midpoint)
+
+## Rotate the grid
+
+library (grid)
+library (gridExtra)
+library (ggpubr)
+
+grid.newpage() 
+pushViewport(viewport(angle=-90, width = unit(8, "inches"), height = unit(8, "inches")))  
+grid.draw(ggplot_gtable(ggplot_build(pall3)))
+
+pdf (paste (gene, "version 6 rotated plot.pdf"), height = 8, width = 8)
+grid.newpage() 
+pushViewport(viewport(angle=-90, width = unit(8, "inches"), height = unit(8, "inches")))  
+grid.draw(ggplot_gtable(ggplot_build(pall3)))
+dev.off ()
+
+
+
+
+
+
 
 
 
